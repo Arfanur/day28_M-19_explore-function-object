8bd9a42f2777aa9f27b7ed862746c66f5a557225
Arfanur Rahman <arfanur03@gmail.com>
2022-02-02 18:36:49 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,6 @@
+var favoriteBook = '4 hour work week';
+var bookList = ['positioning', 'hooked', 'start with why', 'shoe dog'];
+var shoeDogIndex = bookList.indexOf('shoe dog');
+bookList[1] = 'Story Brand';
+
+bookList.push('Small Giants');
