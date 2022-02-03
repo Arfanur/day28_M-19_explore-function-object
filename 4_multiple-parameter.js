a1380fa98896caa778d0b02a567d3c3da4fe85ad
Arfanur Rahman <arfanur03@gmail.com>
2022-02-03 22:12:47 +0600

    4_multiple-parameter

---
+++ 4_multiple-parameter.js
@@ -0,0 +1,39 @@
+// Addition
+function addTwoNumbers(number1, number2,) {
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+}
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(secondNumber, firstNumber);
+console.log('result value: ', result);
+
+// Multiplication
+function multiplyTwoNumbers(num1, num2){
+    console.log(5, 100);
+    var result = num1 * num2;
+    return result;
+}
+var total = multiplyTwoNumbers(5, 100);
+console.log('total after multiplication', total);
+
+// Subtruction
+function subtructionTwoNumber(num1, num2){
+    console.log('Numbers:', num1, num2);
+    var substruction = num1 - num2;
+    return substruction;
+}
+var result = subtructionTwoNumber(33, 3);
+console.log('total after subtruction:', result);
+
+// Division
+function divisionTwoNumber (num1, num2) {
+    console.log('Numbers:', num1, num2);
+    var division = num1 / num2;
+    return division;
+}
+var firstNumber = 44;
+var secondNumber = 4;
+var result = divisionTwoNumber (firstNumber, secondNumber);
+console.log("total after division:", result);
